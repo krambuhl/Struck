b6c601a5ba261d72c18bfd2b549839207b862e64
Evan Krambuhl <thekrambuhl@gmail.com>
2014-06-10 23:58:15 -0700

    updates to intercom idea

--- source/intercom.js
+++ source/intercom.js
@@ -1,12 +1,30 @@
 Struck.Intercom = function () {
-	function Intercom() {
+	var defaultMessage = {
+		name: "all",
 
+	};
+
+	function Intercom() {
+		this.subscribers = [];
 	}
 
-	Intercom.prototype.on = _.noop;
+
+	function splitName(name) {
+		names = _.result(name);
+		var names = _.isArray(name) ? name.split()
+	}
+
+
+	Intercom.prototype.on = function (name, callback, context) {
+
+	};
+
 	Intercom.prototype.once = _.noop;
 	Intercom.prototype.off = _.noop;
 	Intercom.prototype.emit = _.noop;
+	Intercom.prototype.stop = _.noop;
+
+	Intercom.extend = Struck.extend;
 
 	return Intercom;
 }();
