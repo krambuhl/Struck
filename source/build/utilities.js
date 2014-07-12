ec3242db8275522b21169d30edc77037c38749d7
Evan Krambuhl <thekrambuhl@gmail.com>
2014-07-11 23:53:56 -0700

    generalized splitname

--- source/build/utilities.js
+++ source/build/utilities.js
@@ -1,3 +1,16 @@
 function capitalize(string) {
 	return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+// #####splitName
+// split "event1 event2" into an
+// array of event names
+function splitName(name, context) {
+	if (_.isUndefined(context)) context = this;
+	// get result of name if defined as a function
+	var result = _.isFunction(name) ? name.call(context) : name;
+
+	// split by spaces if result isn't an array
+	// always returns an array
+	return _.isArray(result) ? result : result.split(" ");
+}
