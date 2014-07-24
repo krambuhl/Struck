3cd25c722c72f1121f6fb05eb0455b7954a0e331
Evan Krambuhl <thekrambuhl@gmail.com>
2014-07-23 23:02:14 -0700

    Update model.js

--- source/model.js
+++ source/model.js
@@ -5,12 +5,25 @@ Struck.Model = function () {
 	var Model = Struck.EventObject.extend({
 		baseInitiation: function(options) {
 			Struck.EventObject.prototype.baseInitiation.apply(this, arguments);
+			
+			this.data = this.options.data || {};
 		}
 	});
 
-	Model.prototype.get = _.noop;
-	Model.prototype.set = _.noop;
-	Model.prototype.data = _.noop;
+	Model.prototype.get = Struck.Hook("get", function(prop) {
+		return this.data[prop];
+	});
+	
+	Model.prototype.set = Struck.Hook("set", function(prop, val) {
+		// if the first arg is an object
+		// update multiple properties
+		if (_.isObject(prop)) {
+			_.extend(this.data, prop);
+		} else {
+			this.data[prop] = val;
+		}
+	});
+	
 
 	return Model;
 }();
