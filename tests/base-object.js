d7c6b253e74e3d3551fed74b115cf9df18bafe8f
Evan Krambuhl <thekrambuhl@gmail.com>
2014-07-29 09:45:37 -0700

    more base object test

--- tests/base-object.js
+++ tests/base-object.js
@@ -1,12 +1,33 @@
-describe("Base Object", function () {
+describe('Base Object', function () {
   var obj;
 
   it('should return a instance using .create() static method', function () {
     obj = Struck.BaseObject.create();
-    obj.should.have.property('uid');r
+    obj.should.be.an.instanceOf(Struck.BaseObject);
   });
 
-// it should call base initiation
-// it should have a hook method
-// it should 
+  it('should return new object definition using .extend() static method', function () {
+    var SubObject = Struck.BaseObject.extend();
+    var instance = SubObject.create();
+    instance.should.be.an.instanceOf(Struck.BaseObject);
+  });
+
+  it('should extend `obj` into definition using .extend(obj) static method', function () {
+    var SubObject = Struck.BaseObject.extend({ type: "test", test: function () {} });
+    var instance = SubObject.create();
+    instance.should.have.property('type', 'test');
+    instance.should.have.property('test');
+    instance.test.should.be.a.Function;
+  });
+
+  it('should be assigned a UID', function () {
+    obj = Struck.BaseObject.create();
+    obj.should.have.property('uid');
+  });
+
+  it('should extend constructor options to instance', function () {
+    obj = Struck.BaseObject.create({ test: 'data' });
+    obj.should.have.property('options');
+    obj.options.should.have.property('test', 'data');
+  });
 });
