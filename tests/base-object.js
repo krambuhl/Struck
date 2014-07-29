f0da316122079ee2accc3bfced19baa4a711d12a
Evan Krambuhl <thekrambuhl@gmail.com>
2014-07-29 09:58:35 -0700

    initialize test

--- tests/base-object.js
+++ tests/base-object.js
@@ -1,11 +1,6 @@
 describe('Base Object', function () {
   var obj;
 
-  it('should return a instance using .create() static method', function () {
-    obj = Struck.BaseObject.create();
-    obj.should.be.an.instanceOf(Struck.BaseObject);
-  });
-
   it('should return new object definition using .extend() static method', function () {
     var SubObject = Struck.BaseObject.extend();
     var instance = SubObject.create();
@@ -20,14 +15,26 @@ describe('Base Object', function () {
     instance.test.should.be.a.Function;
   });
 
+  it('should return a instance using .create() static method', function () {
+    obj = Struck.BaseObject.create();
+    obj.should.be.an.instanceOf(Struck.BaseObject);
+  });
+
+  it('should extend `opts` to instance using .create(opts) static method', function () {
+    obj = Struck.BaseObject.create({ test: 'data' });
+    obj.should.have.property('options');
+    obj.options.should.have.property('test', 'data');
+  });
+
   it('should be assigned a UID', function () {
     obj = Struck.BaseObject.create();
     obj.should.have.property('uid');
   });
 
-  it('should extend constructor options to instance', function () {
-    obj = Struck.BaseObject.create({ test: 'data' });
-    obj.should.have.property('options');
-    obj.options.should.have.property('test', 'data');
+  it('should call initialize method using .create() static method', function (done) {
+    obj = Struck.BaseObject.extend({ initialize: function () {
+      done();
+    }});
+    obj.create();
   });
 });
