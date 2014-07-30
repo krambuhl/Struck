dc373649c85e2519171290232e7c18e54a35676b
Evan Krambuhl <thekrambuhl@gmail.com>
2014-07-29 23:26:04 -0700

    structured tests!

--- tests/base-object.js
+++ tests/base-object.js
@@ -1,40 +1,57 @@
-describe('Base Object', function () {
+describe('BaseObject', function () {
   var obj;
 
-  it('should return new object definition using .extend() static method', function () {
-    var SubObject = Struck.BaseObject.extend();
-    var instance = SubObject.create();
-    instance.should.be.an.instanceOf(Struck.BaseObject);
+  it('should expose `extend` and `create` static methods', function () {
+    Struck.BaseObject.should.have.property('extend');
+    Struck.BaseObject.should.have.property('create');
   });
 
-  it('should extend `obj` into definition using .extend(obj) static method', function () {
-    var SubObject = Struck.BaseObject.extend({ type: "test", test: function () {} });
-    var instance = SubObject.create();
-    instance.should.have.property('type', 'test');
-    instance.should.have.property('test');
-    instance.test.should.be.a.Function;
+  describe('#extend()', function () {
+    it('should return a new object definition', function () {
+      var SubObject = Struck.BaseObject.extend();
+      var instance = SubObject.create();
+
+      SubObject.should.be.type('function');
+      instance.should.be.an.instanceOf(Struck.BaseObject);
+    });
+
+    it('should extend `obj` into new definition', function () {
+      var SubObject = Struck.BaseObject.extend({ type: "test", test: function () {} });
+      var instance = SubObject.create();
+      instance.should.have.property('type', 'test');
+      instance.should.have.property('test');
+      instance.test.should.be.a.Function;
+    });
   });
 
-  it('should return a instance using .create() static method', function () {
-    obj = Struck.BaseObject.create();
-    obj.should.be.an.instanceOf(Struck.BaseObject);
-  });
+  describe('#create()', function () {
+    it('should return a instance on BaseObject', function () {
+      obj = Struck.BaseObject.create();
+      obj.should.be.an.instanceOf(Struck.BaseObject);
+    });
 
-  it('should extend `opts` to instance using .create(opts) static method', function () {
-    obj = Struck.BaseObject.create({ test: 'data' });
-    obj.should.have.property('options');
-    obj.options.should.have.property('test', 'data');
-  });
+    it('should extend `opts` into instance', function () {
+      obj = Struck.BaseObject.create({ test: 'data' });
+      obj.should.have.property('options');
+      obj.options.should.have.property('test', 'data');
+    });
 
-  it('should be assigned a UID', function () {
-    obj = Struck.BaseObject.create();
-    obj.should.have.property('uid');
-  });
+    it('should call initialize method', function (done) {
+      obj = Struck.BaseObject.extend({ initialize: function () {
+        done();
+      }});
+      obj.create();
+    });
 
-  it('should call initialize method using .create() static method', function (done) {
-    obj = Struck.BaseObject.extend({ initialize: function () {
-      done();
-    }});
-    obj.create();
+    it('should assign a UID', function () {
+      obj = Struck.BaseObject.create();
+      obj.should.have.property('uid');
+    });
   });
+  //
+  //
+  // it('', function () {
+  //
+  // });
+
 });
