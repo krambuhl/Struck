2238ec206d9767414fa8db17c8dba35f4ae2345b
Evan Krambuhl <thekrambuhl@gmail.com>
2014-07-31 00:03:44 -0700

    intercom test

--- tests/intercom.test.js
+++ tests/intercom.test.js
@@ -1,10 +1,12 @@
 describe('Struck.Intercom', function () {
   it('should define a default subscription object', function () {
-
+    var instance = Struck.Intercom.create();
+    instance.should.have.property('defaultSubscription');
   });
 
   it('should define a default collection of subscriptions', function () {
-
+    var instance = Struck.Intercom.create();
+    instance.should.have.property('subscriptions');
   });
 
   describe('on()', function () { });
