a806ebe0b61697c1600c21b4cd4d6ecf42527b6c
Evan Krambuhl <thekrambuhl@gmail.com>
2014-08-31 13:51:37 -0700

    code coverage

--- demo.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-
-//app.use(express.static(__dirname)); // Current directory is root
-app.use(express.static(path.join(__dirname, 'example'))); //  "public" off of current is root
-app.listen(process.env.PORT || 8080);
