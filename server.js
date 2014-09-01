2d1caee32e82ba896312bcd1bef514492c0e8452
Evan Krambuhl <thekrambuhl@gmail.com>
2014-09-01 01:57:51 -0700

    server fix

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var path = require('path');
-
-var router = express.Router();
 var app = express();
 
-app.use('/example', express.static(path.join(__dirname, 'example')));
+app.use(express.static(path.join(__dirname, 'example')));
 app.use('/docs', express.static(path.join(__dirname, 'docs'), { index: 'struck.html' }));
 
 app.listen(process.env.PORT || 8080);
