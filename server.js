9521090afe3cc4165a7011d5e206d6a164a87ff8
Evan Krambuhl <thekrambuhl@gmail.com>
2014-09-09 00:44:33 -0700

    remove server, no need

--- server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var path = require('path');
-var app = express();
-
-app.use(express.static(path.join(__dirname, 'example')));
-app.use('/docs', express.static(path.join(__dirname, 'docs'), { index: 'struck.html' }));
-
-app.listen(process.env.PORT || 8080);
