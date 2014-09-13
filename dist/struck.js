d61e8b8e2b128d1fa85231e3697964d10c718716
Evan Krambuhl <thekrambuhl@gmail.com>
2014-09-12 23:31:02 -0700

    figured out the problem, sitll broken.

--- dist/struck.js
+++ dist/struck.js
@@ -133,7 +133,7 @@ Struck.computed = (function () {
     self.props = splitName(_.flatten(_.initial(arguments)));
     self.func = _.last(arguments);
     
-    setup = _.once(setupListeners);
+    var setup = _.once(setupListeners);
 
     return function() {
     	_.bind(setup, undefined, self, this)();
@@ -141,13 +141,14 @@ Struck.computed = (function () {
     	if (!self.cached) {
 				self.cached = self.func.apply(this);
     	}
+
     	return self.cached;
 		};
   }
 
   function setupListeners(comp, obj) {
   	_.each(comp.props, function(property) {
-  		obj.listenTo('set', function() {
+  		obj.listenTo('set:' + property, function() {
   			comp.cached = comp.func.apply(obj);
   		});
   	});
--- source/computed.js
+++ source/computed.js
@@ -5,7 +5,7 @@ Struck.computed = (function () {
     self.props = splitName(_.flatten(_.initial(arguments)));
     self.func = _.last(arguments);
     
-    setup = _.once(setupListeners);
+    var setup = _.once(setupListeners);
 
     return function() {
     	_.bind(setup, undefined, self, this)();
@@ -13,13 +13,14 @@ Struck.computed = (function () {
     	if (!self.cached) {
 				self.cached = self.func.apply(this);
     	}
+
     	return self.cached;
 		};
   }
 
   function setupListeners(comp, obj) {
   	_.each(comp.props, function(property) {
-  		obj.listenTo('set', function() {
+  		obj.listenTo('set:' + property, function() {
   			comp.cached = comp.func.apply(obj);
   		});
   	});
